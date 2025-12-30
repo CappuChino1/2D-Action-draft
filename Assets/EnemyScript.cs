3fc20f3240b1a05cba02796d5bd1022c1cd66f6d
CappuChino1 <113970507+CappuChino1@users.noreply.github.com>
2025-12-30 19:58:34 +0900

    敵、鳥、追加
    - 敵2 "鳥"追加した
    - プレイトラッキング

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float knockbackPower = 0.01f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit Player");
+
+            //  Deal damage
+            IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.TakeDamage(1); // contact damage
+            }
+
+            // 2 Knockback
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 knockbackDir =
+                    (collision.transform.position - transform.position).normalized;
+
+                rb.AddForce(knockbackDir * knockbackPower, ForceMode2D.Impulse);
+            }
+
+            //  hit effect / sound here
+        }
+    }
+
+
+    public class EnemyBehaviour : MonoBehaviour
+    {
+        public int contactDamage = 1;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            IDamageable target = collision.gameObject.GetComponent<IDamageable>();
+
+            if (target != null)
+            {
+                target.TakeDamage(contactDamage);
+            }
+        }
+    }
+
+
+}
